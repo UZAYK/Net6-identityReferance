5cc46c64b95104af63b55909240d4702f66643fb
Uzay KAHRAMAN <uzaykahramann@gmail.com>
2022-05-28 02:43:45 +0300

    Moved into entities library folder

---
+++ Library/KUSYS-Demo.Entities/Concrete/Course.cs
@@ -0,0 +1,9 @@
+﻿using KUSYS_Demo.Entities.Interfaces;
+
+namespace KUSYS_Demo.Entities.Concrete
+{
+    public class Course : BaseEntity
+    {
+        public string CourseName { get; set; }
+    }
+}
---
+++ Library/KUSYS-Demo.Entities/Interfaces/IBaseEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace KUSYS_Demo.Entities.Interfaces
+{
+    public interface IBaseEntity
+    {
+    }
+}
