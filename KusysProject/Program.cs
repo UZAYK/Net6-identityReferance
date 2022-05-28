6db250ecf2861dc13180a9e638e3b9398974cb11
Uzay KAHRAMAN <uzay.kahraman@clockwork.com.tr>
2022-05-29 02:37:17 +0300

    In the project root files.

--- KusysProject/Program.cs
+++ KusysProject/Program.cs
@@ -1,7 +1,16 @@
+using KUSYS_Demo.Business.AutoMapperProfile;
+using KUSYS_Demo.Business.DiContainer;
+using KUSYS_Demo.DataAccess.Concrete.EntityFrameworkCore.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+builder.Services.AddContainerWithDependencies();
+builder.Services.AddDbContext<KusysDemoContext>();
+
+builder.Services.AddAutoMapper(typeof(MapProfile));
 
 var app = builder.Build();
 
@@ -9,7 +18,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
