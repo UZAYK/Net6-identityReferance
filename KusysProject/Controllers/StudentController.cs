65b9c53ea91dee23384a44d230afe1eb99ac0145
Uzay KAHRAMAN <uzaykahramann@gmail.com>
2022-05-28 18:54:07 +0300

    Student Controller was created.

---
+++ KusysProject/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KUSYS_Demo.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(int id)
+        {
+            return View();
+        }
+        
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult List(int id)
+        {
+            return View();
+        }
+    }
+}
