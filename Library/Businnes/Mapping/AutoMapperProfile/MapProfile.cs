8beb0fb1e25e283a07c3fe6b3df6aee31421babb
Uzay KAHRAMAN <uzay.kahraman@clockwork.com.tr>
2022-05-29 02:38:21 +0300

    inclusion of automapper in the project

---
+++ Library/Businnes/Mapping/AutoMapperProfile/MapProfile.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+
+using KUSYS_Demo.Models;
+using KUSYS_Demo.Entities.Concrete;
+using KUSYS_Demo.Models.StudentCourseMap;
+
+namespace KUSYS_Demo.Business.AutoMapperProfile
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            #region Course-CourseModel
+
+            CreateMap<CourseModel, Course>();
+            CreateMap<Course, CourseModel>();
+
+            #endregion
+
+            #region Student-StudentModel
+
+            CreateMap<StudentModel, Student>();
+            CreateMap<Student, StudentModel>();
+
+            #endregion
+
+            #region StudentCourseAddMap-StudentCourseMapModel
+
+            CreateMap<StudentCourseMapAddModel, StudentCourseMap>();
+            CreateMap<StudentCourseMap, StudentCourseMapAddModel>();
+
+            #endregion 
+            
+            #region StudentCourseListMap-StudentCourseMapModel
+
+            CreateMap<StudentCourseMapListModel, StudentCourseMap>();
+            CreateMap<StudentCourseMap, StudentCourseMapListModel>();
+
+            #endregion
+        }
+    }
+}
